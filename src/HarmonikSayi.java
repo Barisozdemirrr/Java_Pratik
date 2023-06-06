02180a1e13a2810edaaddcafcc03fd3115b4ecb2
fnsstalemate <fnsstalemate@gmail.com>
2023-06-06 09:16:53 +0300

    odev

--- src/HarmonikSayi.java
+++ src/HarmonikSayi.java
@@ -4,16 +4,16 @@ public class HarmonikSayi {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int sayi;
-        double toplam=0.0 ;
+        double toplam = 0.0;
         System.out.println("Lüten Harmonik Serisini Bulmak istediginiz Sayıyı Giriniz : ");
-        sayi=input.nextInt();
-        for (double i =1;i<=sayi;i++){
-            System.out.print(1.0+"/"+i);
+        sayi = input.nextInt();
+        for (double i = 1; i <= sayi; i++) {
+            System.out.print(1.0 + "/" + i);
             System.out.print("+");
             System.out.println();
-            toplam+=(1.0/i);
+            toplam += (1.0 / i);
 
         }
-        System.out.println("Sonuc : "+toplam);
+        System.out.println("Sonuc : " + toplam);
     }
 }
