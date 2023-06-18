db40769a15a1ca070c8d2cf4e243ff58dc6acb66
fnsstalemate <fnsstalemate@gmail.com>
2023-06-18 13:34:51 +0300

    Dizi elemanlarını küçükten büyüge sıralama

--- src/Diziler/DiziElemanlariSiralama.java
+++ src/Diziler/DiziElemanlariSiralama.java
@@ -7,15 +7,16 @@ public class DiziElemanlariSiralama {
     public static void main(String[] args) {
         int n;
         Scanner input=new Scanner(System.in);
-        System.out.println("Lütfen dizinin eleman sayısını giriniz : ");
+        System.out.println("Lütfen dizinin eleman sayısını giriniz : "); // Dizinin eleman sayısını kullanıcadan alıyoruz.
         n = input.nextInt();
         int[] array =new int[n] ;
         for (int i =0;i<n;i++){
-            System.out.println("Lütfen dizinin elemanlarını giriniz : ");
+            System.out.println("Lütfen dizinin elemanlarını giriniz : ");//Döngü yardımı ile dizinin elemanlarını kullanıcıdan alıyoruz.
             array[i]=input.nextInt();
 
         }
-        Arrays.sort(array);
-        System.out.println(Arrays.toString(array));
+        Arrays.sort(array);//Array sort metodu ile dizi küçükten büyüge sıralanıyor.
+        System.out.println("Dizinin küçükten büyüğe sıralı hali = "+Arrays.toString(array));//to string metodu ile sıralanmış diziyi ekrana bastrıyoruz.
+
     }
 }
